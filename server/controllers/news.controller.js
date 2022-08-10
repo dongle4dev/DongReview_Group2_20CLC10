9980b4b23c0c0196cdd01fb9caa099419f09de63
DongEmSi <ledongdong102@gmail.com>
2022-08-10 22:27:39 +0700

    Chỉnh sửa tính năng login, login validation, update film, update news

--- server/controllers/news.controller.js
+++ server/controllers/news.controller.js
@@ -3,23 +3,13 @@ const { multiMongooseToObject } = require('../util/mongoose')
 const { mongooseToObject } = require('../util/mongoose')
 
 class NewsController {
-    index(req, res, next) {
-        News.find({})
-            .then(news => res.json(news))
-            .catch(next);
-    }
-
-    //[GET] /news/:slug
+    //[GET] /news/:filmID
     show(req, res, next) {
-        News.findOne({slug: req.params.slug}) 
-            .then(news => {
-                res.render('news/show', {
-                    news:  mongooseToObject(news)
-                });
-            })
+        News.find({filmID: req.params.filmID}) 
+            .then(news => res.json({status: 'Ok', elements: news}))
             .catch(next);
     }
-
+    //[POST] /news/store
     async store(req, res, next) {
         try {
             const news = new News(req.body)
@@ -33,6 +23,12 @@ class NewsController {
             next(err)
         }
 
+    }
+    //[PUT] /news/:id
+    update(req, res, next) {
+        Film.updateOne({_id: req.params.id}, req.body)
+            .then((news) => res.json({status: 'Ok', elements: news}))
+            .catch(next)
     }
 }
 
